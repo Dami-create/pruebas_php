b6e9ba4bba9e69f78c46a3a3c5a95cc8d5395874
Dami-create <dabansa1109@gmail.com>
2021-04-26 20:33:42 -0300

    good change!

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php
  
-    echo "HOLA MUNDO !!!!";
+    echo "Comenzamos a realizar el debug...";
+
+    $valor="Hola Mundo";
+
+    $valor = 12;
+
+
+
+    function una_prueba(&$val){
+
+        $val = 5;
+
+        return "hola";
+    }
+
+    
+    $val = 3;
+    una_prueba($val);
+
+
+
+
+    //public void una_prueba(&$val)
+
+    $val = $val+2;
+    echo "Valor: " . $val;
+
+
+
+    $var1 = 45;
+    
+    $var2 = 45;
+
+
