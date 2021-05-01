2cd7fdc5f8fede53189bc12921beca723e9cd108
Dami-create <dabansa1109@gmail.com>
2021-05-01 19:15:23 -0300

    changed

--- teorico.php
+++ teorico.php
@@ -5,4 +5,12 @@ $b = $a."job";
 
 echo "$b";
 
+
+$foo = 'Bob'; // Asigna el valor 'Bob' a $foo
+$bar = &$foo; // Referenciar $foo vía $bar.
+$bar = "Mi nombre es $bar"; // Modifica $bar...
+echo $bar;
+echo $foo; // $foo también se modifica.
+
+
 ?>
