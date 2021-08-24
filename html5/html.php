abcb35d88be31468e7dba32da135c720cf452750
Dami-create <dabansa1109@gmail.com>
2021-08-24 14:46:04 -0300

    ajax

--- html5/html.php
+++ html5/html.php
@@ -4,17 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--link rel="stylesheet" href="../css/estilos.css"-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilos.css">
+    <!--link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"-->
     <title>Document</title>
 
     </head>
     <body>
-    
 
-    
-    
-    
+    <!-- <p> ESTO ES UN PARRAFO </p>
+    <p class="p_rojos"> ESTO ES UN PARRAFO </p>
+    <p> ESTO ES UN PARRAFO </p>
+    <p id="p_rosa"> ESTO ES UN PARRAFO </p> -->
+
 
     <div id="capa1" class="capa texto_capa">
 
